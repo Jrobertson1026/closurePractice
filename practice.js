5ce06f78478695ef3843aaa9a264c9a3a6e3197a
Jacob Robertson <jwrob1026@gmail.com>
2015-04-09 13:38:52 -0600

    closurePractice code

--- practice.js
+++ practice.js
@@ -1,19 +1,19 @@
 //Once you complete a problem, open up Chrome and check the answer in the console.
 
-var outer = function(){
-  var name = 'Tyler';
-  return function(){
-    return 'The original name was ' + name;
+var outer = function() {
+    var name = 'Tyler';
+    return function() {
+      return 'The original name was ' + name;
+    }
   }
-}
-//Above you're given a function that returns another function which has a closure over the name variable.
-//Invoke outer saving the return value into another variable called 'inner'.
+  //Above you're given a function that returns another function which has a closure over the name variable.
+  //Invoke outer saving the return value into another variable called 'inner'.
 
-  //Code Here
+var inner = outer();
 
 //Once you do that, invoke inner.
 
-  //Code Here
+inner();
 
 
 
@@ -21,9 +21,10 @@ var outer = function(){
 
 
 
-var callFriend = function(){
+var callFriend = function() {
   var friend = 'Jake';
-  function callF(number){
+
+  function callF(number) {
     return 'Calling ' + friend + ' at ' + number;
   }
   return callF;
@@ -32,24 +33,30 @@ var callFriend = function(){
 //Above you're given a callFriend function that returns another function.
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
-  //Code Here
 
 
 
-//Next Problem
 
+var calling = callFriend();
+console.log(calling("435-215-9248"));
 
 
 /*
   Write a function called makeCounter that makes the following code work properly.
 */
 
-  //Code Here
-  var count = makeCounter();
-  count() // 1
-  count() // 2
-  count() // 3
-  count() // 4
+var makeCounter = function() {
+  var number = 0;
+
+  function bubbles() {
+    return number++;
+  }
+};
+var count = makeCounter();
+count() // 1
+count() // 2
+count() // 3
+count() // 4
 
 
 
@@ -63,7 +70,20 @@ var callFriend = function(){
   Once completed, add a second arguments that allows the function to be invoked N number of times.
   After the function has been called N number of times, console.log('STAHHP');
 */
+var writedFn = function(fn, n) {
+  var count = 0;
+  return function() {
+    if (count < n) {
+      count++;
+      fn();
+    } else {
+      console.log("STAHHP")
+    }
+
+  }
+};
 
 
-
-
+var welcome = function() {
+  alert("This is Awesome!!!")
+}
